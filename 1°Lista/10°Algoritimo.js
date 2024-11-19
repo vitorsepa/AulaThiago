384a31fa560d7dcacfe62615d93e084d4bfbdd31
vitorsepa <162443534+vitorsepa@users.noreply.github.com>
2024-11-19 12:31:35 -0300

    Delete 1°Lista directory

--- 1°Lista/10°Algoritimo.js
+++
@@ -1,7 +0,0 @@
-/*
- Crie um vetor de 10 números e peça para o usuário qual número ele deseja verificar se existe no vetor. 
- Caso exista, o programa deve mostrar todos os índices que ele se encontra.
-
- Dicas: Use dois vetores - Use um laços de repetição
-*/
-
